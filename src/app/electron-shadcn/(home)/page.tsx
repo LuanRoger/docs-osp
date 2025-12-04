6a35daddc9bdf29d8fc25cfb2cb6bd8955ba5bf5
ROG <luan.roger.2003@gmail.com>
2025-12-04 10:57:52 -0300

    feat: add icons to home page

--- src/app/electron-shadcn/(home)/page.tsx
+++ src/app/electron-shadcn/(home)/page.tsx
@@ -1,4 +1,7 @@
 import Link from "next/link";
+import ElectronIcon from "@/components/icons/electron";
+import ReactIcon from "@/components/icons/react";
+import ViteIcon from "@/components/icons/vite";
 import Prism from "@/components/prism";
 import Reveal from "@/components/reveal";
 import { Button } from "@/components/ui/button";
@@ -7,21 +10,28 @@ import { mountRoute } from "@/lib/utils/route";
 
 export default function HomePage() {
   return (
-    <div className="relative inset-shadow-white flex-1 rounded-2xl border border-border">
-      <Reveal className="absolute inset-0 blur-sm">
+    <div className="relative inset-shadow-sm flex-1 overflow-hidden rounded-2xl border border-border">
+      <Reveal className="absolute inset-0 hidden translate-y-20 lg:block">
         <Prism
           animationType="rotate"
-          baseWidth={8}
+          baseWidth={10}
           colorFrequency={1}
           glow={0.3}
           height={8}
           hueShift={0}
           noise={0}
-          scale={2}
+          scale={1}
           timeScale={0.2}
         />
       </Reveal>
       <div className="absolute inset-0 flex flex-col items-center justify-center gap-2">
+        <div className="flex flex-col items-center gap-2">
+          <ElectronIcon className="size-16 fill-[#47848F]" />
+          <div className="flex gap-2">
+            <ViteIcon className="size-16 fill-[#646CFF]" />
+            <ReactIcon className="size-16 fill-[#61DAFB]" />
+          </div>
+        </div>
         <h1 className="font-bold text-3xl">electron-shadcn</h1>
         <p className="font-medium text-lg">
           Electron Forge with shadcn-ui (Vite + Typescript){" "}
